b4707b66e2be42b5b41add3ba27eab3887c3ce37
DKMFzF <flim.win@yandex.ru>
2025-11-13 16:08:29 +0700

    fix: clean.sh

--- backend/bin/clear.sh
+++ backend/bin/clear.sh
@@ -1,12 +1,7 @@
-#!/bin/build
-
-echo "test..1"
+#!/bin/bash
 
 if [ -d "./build" ]; then
 	echo "delete builds...."
-	
-	echo "test..2"
-
 	sudo rm -rf ./build/main*
 else
 	echo "build folder not found"
